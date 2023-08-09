766eae9226652569e3f924f66d38cdd67efb1be2
welington.souza <welington.souza@mprj.mp.br>
2023-08-09 16:51:13 +0000

    Reformando o models com base no black.

--- filtro/models.py
+++ filtro/models.py
@@ -27,15 +27,17 @@ TIPOS_FILTRO = (
     ('5', 'Termos de Reforço'),
 )
 
-CNPJ_CPF = (('02421421000111', '02421421000111'),
-('00000000000191', '00000000000191'), ('00000000000515', '00000000000515'), ('00000000004774', '00000000004774'),
-('00000000007285', '00000000007285'), ('00000000007447', '00000000007447'), ('00000000008176', '00000000008176'),
-('00000000008761', '00000000008761'), ('00000000009148', '00000000009148'), ('00000000011398', '00000000011398'),
-('00000000012793', '00000000012793'), ('00000000013170', '00000000013170'), ('00000000015032', '00000000015032'),
-('00000000024961', '00000000024961'), ('00000000025852', '00000000025852'), ('00000000026239', '00000000026239'),
-('00000000028797', '00000000028797'), ('00000000028878', '00000000028878'), ('00000000029688', '00000000029688'),
-('00000000031232', '00000000031232'), ('00000000039217', '00000000039217'), ('00000000039489', '00000000039489'),
-('00000000041890', '00000000041890'), ('00000000043591', '00000000043591'), ('00000000046930', '00000000046930'),)
+CNPJ_CPF = (
+    ('02421421000111', '02421421000111'), ('00000000000191', '00000000000191'), ('00000000000515', '00000000000515'),
+    ('00000000004774', '00000000004774'), ('00000000007285', '00000000007285'), ('00000000007447', '00000000007447'),
+    ('00000000008176', '00000000008176'), ('00000000008761', '00000000008761'), ('00000000009148', '00000000009148'),
+    ('00000000011398', '00000000011398'), ('00000000012793', '00000000012793'), ('00000000013170', '00000000013170'),
+    ('00000000015032', '00000000015032'), ('00000000024961', '00000000024961'), ('00000000025852', '00000000025852'),
+    ('00000000026239', '00000000026239'), ('00000000028797', '00000000028797'), ('00000000028878', '00000000028878'),
+    ('00000000029688', '00000000029688'), ('00000000031232', '00000000031232'), ('00000000039217', '00000000039217'),
+    ('00000000039489', '00000000039489'), ('00000000041890', '00000000041890'), ('00000000043591', '00000000043591'),
+    ('00000000046930', '00000000046930'),
+)
 
 
 class TipoMovimento(models.Model):
@@ -49,29 +51,15 @@ class TipoMovimento(models.Model):
 
 class Filtro(models.Model):
     nome = models.CharField(max_length=50)
-    tipo_raspador = models.CharField(
-        max_length=1,
-        choices=TIPOS_RASPADOR,
-        blank=True,
-        null=True
-    )
+    tipo_raspador = models.CharField(max_length=1, choices=TIPOS_RASPADOR, blank=True, null=True)
     tipos_movimento = models.ManyToManyField('TipoMovimento', blank=True)
     arquivo_documentos = models.FileField(null=True, blank=True)
-    situacao = models.CharField(
-        max_length=1,
-        choices=SITUACOES_FILTRO,
-        default='1'
-    )
+    situacao = models.CharField(max_length=1, choices=SITUACOES_FILTRO, default='1')
     saida = models.FileField(null=True, blank=True, storage=OverwriteStorage())
     saida_lda = models.TextField(null=True, blank=True)
     responsavel = models.CharField(max_length=50, default='')
     percentual_atual = models.FloatField(null=True, blank=True)
-    reu = models.CharField(
-        max_length=100,
-        choices=CNPJ_CPF,
-        blank=True,
-        null=True
-    )
+    reu = models.CharField(max_length=100, choices=CNPJ_CPF, blank=True, null=True)
 
     def __str__(self):
         if self:
@@ -111,18 +99,8 @@ class ItemFiltro(models.Model):
 
 
 class Documento(models.Model):
-    classe_filtro = models.ForeignKey(
-        'ClasseFiltro',
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE
-    )
+    classe_filtro = models.ForeignKey('ClasseFiltro', blank=True, null=True, on_delete=models.CASCADE)
     filtro = models.ForeignKey('Filtro', on_delete=models.CASCADE)
     numero = models.CharField(max_length=32)
-    tipo_movimento = models.ForeignKey(
-        'TipoMovimento',
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True
-    )
+    tipo_movimento = models.ForeignKey('TipoMovimento', on_delete=models.CASCADE, blank=True, null=True)
     conteudo = models.TextField()
